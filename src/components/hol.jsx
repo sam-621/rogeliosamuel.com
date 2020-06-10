f36a1cc4afa21fd50286b38905f3715b79a8a7e4
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2020-06-10 14:07:18 -0600

    Adde all config to work with react without create-react-app

---
+++ src/components/hol.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../assets/styles/hol.css'
+
+const Hola = () => {
+    return(
+        <h1>Hola mundo</h1>
+    );
+}
+export default Hola;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Hola from './components/hol';
+
+ReactDOM.render(<Hola/>, document.getElementById('app'))
