1676a1b188e5a8f2d77681c95abecd4d067d4d84
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2020-07-07 00:22:54 -0600

    Finishing the design card

--- src/components/FeaturedProjectCard.jsx
+++ src/components/FeaturedProjectCard.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import '../assets/styles/featuredProjectCard.css';
 
+import Arrow from '../assets/img/flecha.svg';
+
 const FeaturedProjectCard = ({ index, title, image }) => {
     return(
         <article className="FeaturedProjectCard">
-            <div className="Title">
-                <h3>{title}</h3>
+            <div className="Options">
+                <div className="Title">
+                    <h3>{title}</h3>
+                </div>
+                <div className="ViewProfile">
+                    <a href="/">
+                        <img src={Arrow} alt="" width="30px" height="30px" />
+                    </a>
+                </div>
             </div>
             <div className="img">
-                <img src={image} alt="" width="488" height="217"/>
+                <img src={image} alt="" width="486" height="217"/>
             </div>  
         </article>
     );
