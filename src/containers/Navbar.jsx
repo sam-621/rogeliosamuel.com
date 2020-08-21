0457c7aa695c731d381919ac77f7d02c78637b6c
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2020-08-21 16:38:11 -0600

    [UPDATE] My portfolio without create-react-app

--- src/containers/Navbar.jsx
+++ src/containers/Navbar.jsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import '../assets/styles/navBar.css'
+import React from "react";
+import "../assets/styles/navBar.css";
 
-import Hero from '../components/Hero';
-import Nav from '../components/Nav';
+import Hero from "../components/Hero";
+import Nav from "../components/Nav";
 
 const NavBar = ({ location }) => {
-    return(
-        <nav>
-            <section>
-                <Hero />
-            </section>
-            <section>
-                <Nav 
-                    inProject={location === 'inProject' ? true : false} 
-                    inMain={location === 'inMain' ? true : false} 
-                    inAboutMe={location === 'inAboutMe' ? true : false} 
-                />
-            </section>
-        </nav>
-    );
-}
+  return (
+    <nav>
+      <section>
+        <Hero />
+      </section>
+      <section>
+        <Nav
+          inProject={location === "inProject" ? true : false}
+          inMain={location === "inMain" ? true : false}
+          inAboutMe={location === "inAboutMe" ? true : false}
+        />
+      </section>
+    </nav>
+  );
+};
 
 export default NavBar;
---
+++ webpack.config.prod.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "src/index.jsx"),
+  output: {
+    filename: "bundle.[hash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+        resolve: {
+          extensions: [".js", ".jsx"],
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          "css-loader",
+        ],
+      },
+      {
+        test: /\.(png|jpg|svg)$/i,
+        loader: "file-loader",
+        options: {
+          outputPath: "img",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public/index.html"),
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+};
