115af161e5303ffb74ae6c22d4f215b03f83d9b0
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2020-08-24 16:23:18 -0600

    [ADD] Projects slides

--- src/assets/FeaturedProjects.js
+++ src/assets/FeaturedProjects.js
@@ -1,9 +1,29 @@
 export const FeaturedProjectsData = [
   {
     ID: 1,
-    title: "Title project",
+    title: "SocialMedia-API",
     littleDescription:
-      "It is a videogame where you have to destroy all enemies. it has 2 levels, the last one has a boss. Enjoy",
-    technologies: ["React.js", "Node.js", "CSS", "Typescript", "MySQL"],
+      "An API Created in order to practice my backend skills creating a social media backend",
+    technologies: ["Node.js", "Typescript", "MySQL"],
+    Repo: "https://github.com/rogeliosamuel621/Social-Media-API",
+    demo: "https://github.com/rogeliosamuel621/Social-Media-API",
+  },
+  {
+    ID: 2,
+    title: "Safe-Contacts-API",
+    littleDescription:
+      "An API for a web aplication that i create time ago. This API is to learn and practice Typescript",
+    technologies: ["Node.js", "Typescript", "MySQL"],
+    Repo: "https://github.com/rogeliosamuel621/Safe-contacts-API",
+    demo: "https://github.com/rogeliosamuel621/Safe-contacts-API",
+  },
+  {
+    ID: 3,
+    title: "Jobs-Listing",
+    littleDescription:
+      "Project created in order to practice Reactjs. Challenge from frontendmentor",
+    technologies: ["React.js", "Javascript", "Webpack"],
+    Repo: "https://github.com/rogeliosamuel621/Jobs-Listing",
+    demo: "https://rogeliosamuel621.github.io/Jobs-Listing/",
   },
 ];
--- src/containers/FeaturedProjects.jsx
+++ src/containers/FeaturedProjects.jsx
@@ -1,12 +1,61 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../assets/styles/containers/FeaturedProjects.css";
 
 import { FeaturedProjectsData } from "../assets/FeaturedProjects";
 import SecondaryText from "../components/atoms/text/SecondaryText";
 import Arrow from "../components/atoms/img/Arrows";
 import FeaturedProjectDescription from "../components/molecules/FeaturedProjectDescription";
+import SocialMediaAPI from "../assets/img/SocialMedia-API.png";
+import SafeContactAPi from "../assets/img/safecontact-api.png";
+import JobsListing from "../assets/img/jobs-listing.jpg";
 
 const FeaturedProjects = () => {
+  const img = [SocialMediaAPI, SafeContactAPi, JobsListing];
+  var slide1;
+  var slide2;
+  var slide3;
+  var slides = [];
+  var index = 0;
+
+  useEffect(() => {
+    slide1 = document.getElementById("0");
+    slide2 = document.getElementById("1");
+    slide3 = document.getElementById("2");
+    slides = [slide1, slide2, slide3];
+    index = 0;
+  });
+
+  function Next() {
+    slides[0].style.display = "none";
+    slides[1].style.display = "none";
+    slides[2].style.display = "none";
+
+    index++;
+
+    if (index === 3) {
+      index = 0;
+      slides[index].style.display = "grid";
+      return;
+    }
+
+    slides[index].style.display = "grid";
+  }
+
+  function Back() {
+    slides[0].style.display = "none";
+    slides[1].style.display = "none";
+    slides[2].style.display = "none";
+
+    index--;
+
+    if (index === -1) {
+      index = 2;
+      slides[index].style.display = "grid";
+      return;
+    }
+
+    slides[index].style.display = "grid";
+  }
   return (
     <main className="FeaturedProjects-container">
       <div className="FeaturedProjets-title">
@@ -14,27 +63,54 @@ const FeaturedProjects = () => {
       </div>
       <div className="FeaturedProjects">
         <div className="Arrow-container">
-          <button>
+          <button onClick={Back}>
             <Arrow Direction="left" />
           </button>
         </div>
-        <div className="FeaturedProject-content">
+        <div>
           <div>
             {FeaturedProjectsData.map((project, index) => {
-              return (
-                <FeaturedProjectDescription
+              return index > 0 ? (
+                <div
+                  className="FeaturedProject-content none"
+                  id={index}
                   key={project.ID}
-                  littleDescription={project.littleDescription}
-                  technologies={project.technologies}
-                  title={project.title}
-                />
+                >
+                  <div>
+                    <FeaturedProjectDescription
+                      littleDescription={project.littleDescription}
+                      technologies={project.technologies}
+                      title={project.title}
+                    />
+                  </div>
+                  <div className="FeaturedProject-img">
+                    <img src={img[index]} alt="" width="569px" height="300px" />
+                  </div>
+                </div>
+              ) : (
+                <div
+                  className="FeaturedProject-content"
+                  id="0"
+                  key={project.ID}
+                >
+                  <div>
+                    <FeaturedProjectDescription
+                      key={project.ID}
+                      littleDescription={project.littleDescription}
+                      technologies={project.technologies}
+                      title={project.title}
+                    />
+                  </div>
+                  <div className="FeaturedProject-img">
+                    <img src={img[index]} alt="" width="569px" height="300px" />
+                  </div>
+                </div>
               );
             })}
           </div>
-          <div></div>
         </div>
         <div className="Arrow-container">
-          <button>
+          <button onClick={Next}>
             <Arrow Direction="right" />
           </button>
         </div>
