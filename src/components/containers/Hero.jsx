4254d346a0239daccdc17551ec3ccd7bef715b47
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2020-08-29 14:12:59 -0600

    [CREATE] Hero

---
+++ src/components/containers/Hero.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "../../assets/styles/containers/hero.css";
+
+import Title from "../atoms/text/Title";
+import Me from "../../assets/img/Yo.jpg";
+
+const Hero = () => {
+  return (
+    <div className="Hero-container">
+      <Title Content="Im a Backend developer" Center={true} />
+      <div className="CodeText">
+        <p>
+          Backend developer. Typescript, MySQL. Open to working with other
+          people
+        </p>
+      </div>
+      <div className="Hero-MyImg">
+        <img
+          src={Me}
+          alt="Rogelio Samuel moreno Corrales"
+          width="200px"
+          height="315px"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 import Nav from "../components/containers/Nav";
-
+import Hero from "../components/containers/Hero";
 const Home = () => {
   return (
     <>
       <Nav />
+      <Hero />
     </>
   );
 };
