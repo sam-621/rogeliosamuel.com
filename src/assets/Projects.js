8471230ef33372a6b827acfca703453304e75c0b
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2020-08-30 13:11:10 -0600

    [UPDATE] Styles.

--- src/assets/Projects.js
+++ src/assets/Projects.js
@@ -15,20 +15,6 @@ export const ProjectsData = [
   },
   {
     ID: 1,
-    title: "Safe-Contacts-API",
-    littleDescription:
-      "An API for a web aplication that i create time ago. This API is to learn and practice Typescript",
-    technologies: [
-      { icon: 0, title: "Node.js" },
-      { icon: 1, title: "Typescript" },
-      { icon: 2, title: "MySQL" },
-    ],
-    Repo: "https://github.com/rogeliosamuel621/Safe-contacts-API",
-    demo: "https://github.com/rogeliosamuel621/Safe-contacts-API",
-    features: ["Some", "some1"],
-  },
-  {
-    ID: 2,
     title: "Jobs-Listing",
     littleDescription:
       "Project created in order to practice Reactjs. Challenge from frontendmentor",
@@ -39,6 +25,20 @@ export const ProjectsData = [
     ],
     Repo: "https://github.com/rogeliosamuel621/Jobs-Listing",
     demo: "https://rogeliosamuel621.github.io/Jobs-Listing/",
+    features: ["Some", "some1"],
+  },
+  {
+    ID: 2,
+    title: "Safe-Contacts-API",
+    littleDescription:
+      "An API for a web aplication that i create time ago. This API is to learn and practice Typescript",
+    technologies: [
+      { icon: 0, title: "Node.js" },
+      { icon: 1, title: "Typescript" },
+      { icon: 2, title: "MySQL" },
+    ],
+    Repo: "https://github.com/rogeliosamuel621/Safe-contacts-API",
+    demo: "https://github.com/rogeliosamuel621/Safe-contacts-API",
     features: ["Some", "some1"],
   },
 ];
--- src/components/molecules/project.jsx
+++ src/components/molecules/project.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import "../../assets/styles/molecules/project.css";
 
+import JobsListing from "../../assets/img/jobs-listing.jpg";
+import SafeContactsAPI from "../../assets/img/safecontact-api.png";
 import PrimaryLink from "../atoms/btn/PrimaryLink";
 
 const Project = ({ ProjectTitle, Description, ID }) => {
+  const projectsImages = [null, JobsListing, SafeContactsAPI];
   return (
     <div className="Project-container">
       <div className="Project-title">
         <h2>{ProjectTitle}</h2>
       </div>
       <div className="project-description">
-        <p>{Description}</p>
+        <img
+          src={projectsImages[ID]}
+          alt="Project image"
+          width="250px"
+          height="104.59px"
+        />
       </div>
       <div className="project-btn">
         <PrimaryLink Content="Details" Url={`/${ID}`} />
