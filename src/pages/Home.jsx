a46926d4748cfd42f2087d24b681e88e99ca26d7
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2020-08-29 21:05:59 -0600

    [ADD] Skills component.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import Nav from "../components/containers/Nav";
 import Hero from "../components/containers/Hero";
 import Projects from "../components/containers/Projects";
+import Skills from "../components/containers/Skills";
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <Nav />
       <Hero />
       <Projects />
+      <Skills />
     </>
   );
 };
