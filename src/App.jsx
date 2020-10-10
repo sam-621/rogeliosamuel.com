cdfd9b9df2f7edb2c344ded8dd76c2710896be8f
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2020-10-10 13:01:25 -0600

    [REMOVE] Folders.

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,12 @@ import "./assets/styles/app.css";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
-import AboutProject from "./pages/AboutProject";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/:projectID" component={AboutProject} />
       </Switch>
     </BrowserRouter>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,9 @@
 import React from "react";
 
-import Nav from "../components/containers/Nav";
-import Hero from "../components/containers/Hero";
-import Projects from "../components/containers/Projects";
-import Footer from "../components/containers/Footer";
-import Info from "../components/containers/Info";
-
 const Home = () => {
   return (
     <>
-      <Nav />
-      <Hero />
-      <Projects />
-      <Info />
-      <Footer />
+      <h1>Hello wordl</h1>
     </>
   );
 };
