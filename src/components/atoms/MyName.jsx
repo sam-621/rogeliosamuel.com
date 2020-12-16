808f4c8a71e02d659f5d67f2cbef3c11e39fc530
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2020-12-15 18:05:10 -0700

    (add) my logo in svg

--- src/components/atoms/MyName.jsx
+++ src/components/atoms/MyName.jsx
@@ -2,19 +2,15 @@ import React, { useContext } from 'react';
 import '../../styles/atoms/MyName.css';
 
 import ColorTheme from '../../context/ColorTheme';
+import MyLogo from '../../assets/img/myLogo.svg';
 import { Link } from 'react-router-dom';
 
 const MyName = () => {
   const { theme } = useContext(ColorTheme);
   const color = theme ? 'White' : 'Dark';
   return (
-    <Link to="/" className={`MyName`}>
-      <img
-        src="https://res.cloudinary.com/dnvp4s8pe/image/upload/v1605550009/My%20Website/pltdoxjom4pvhfqwopwp.jpg"
-        alt="Rogelio Samuel Logo"
-        width="40px"
-        height="40px"
-      />
+    <Link to="/" className="MyName">
+      <img src={MyLogo} alt="Rogelio Samuel Logo" width="40px" height="40px" />
       <h1 className={`MyName-Text-${color}`}>Rogelio Samuel</h1>
     </Link>
   );
