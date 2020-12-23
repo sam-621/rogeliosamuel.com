c1cbcf5f9b9a4d789d0c5941dee201a349829dfd
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2020-12-22 21:50:18 -0700

    (add) dark and white mode

--- src/components/containers/RecentBlogs.jsx
+++ src/components/containers/RecentBlogs.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import '../../styles/containers/RecentBlogs.css';
 
 import { Title } from '../atoms';
--- src/components/molecules/Blog.jsx
+++ src/components/molecules/Blog.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import '../../styles/molecules/Blog.css';
 
+import ColorTheme from '../../context/ColorTheme';
+
 const Blog = ({ img, title, date, url }) => {
+  const { theme } = useContext(ColorTheme);
+  const color = theme ? 'White' : 'Dark';
   return (
     <article className="Blog-article">
       <a href={url} target="_blank" className="Blog">
         <div className="Blog-image">
           <img src={img} alt="blog icon" width="60px" height="60px" />
         </div>
-        <div className="Blog-text">
+        <div className={`Blog-text Blog-text-${color}`}>
           <h2>{title}</h2>
           <p>{date}</p>
         </div>
