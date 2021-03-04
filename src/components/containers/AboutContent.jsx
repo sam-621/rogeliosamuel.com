22293c878a26a36ec53a508ddbcdb0ed680a874b
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-03-04 16:58:52 -0700

    add fist section to about content

--- src/components/containers/AboutContent.jsx
+++ src/components/containers/AboutContent.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import '../../styles/containers/aboutContent.css';
 import { Me } from '../atoms';
 
 const AboutContent = () => {
   return (
-    <main>
-      <Me />
+    <main className="AboutContent">
+      <div className="AboutContent-first">
+        <Me />
+        <h1>Hello im Rogelio Samuel</h1>
+        <p>
+          Web developer, intern at EDteam, MERN stack. Always learning and open
+          to working with other people.
+        </p>
+      </div>
     </main>
   );
 };
