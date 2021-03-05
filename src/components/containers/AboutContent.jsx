c550567e4f3d6bc8ade77ed6c41abda8cd32bd9f
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-03-04 17:14:58 -0700

    add styles for about content

--- src/components/containers/AboutContent.jsx
+++ src/components/containers/AboutContent.jsx
@@ -10,7 +10,7 @@ const AboutContent = () => {
         <Me />
         <h1>Hello im Rogelio Samuel</h1>
         <p>
-          Web developer, intern at EDteam, MERN stack. Always learning and open
+          Web developer, intern at @EDteam, MERN stack. Always learning and open
           to working with other people.
         </p>
       </div>
@@ -18,26 +18,31 @@ const AboutContent = () => {
         <h2>Who am i?</h2>
         <p>
           A developer from mexico who likes math and programming, im 18 years
-          old, currently im learning web development but i’d like to learn about
-          data science and machine learning. I enjoy teaching my knowledge and
-          working with other people. i have a blog and im working in T-Rex Codes
-          :)
+          old, currently im learning web development. <br /> <br /> I enjoy
+          teaching my knowledge and working with other people. i have a{' '}
+          <a href="https://dev.to/rogeliosamuel621" target="_blank">
+            blog
+          </a>{' '}
+          and also write code with friends in{' '}
+          <a href="https://github.com/trexcodes" target="_blank">
+            T-Rex Codes
+          </a>
         </p>
       </div>
       <div className="AboutContent-skills">
         <h2>Tecnical skills</h2>
         <p>
-          I work with MERN stack but also i know other tecnologies and other
-          skills thant just coding
+          I work with <a href="">MERN</a> stack but also i know other
+          tecnologies and other skills thant just coding
         </p>
         <Skills />
       </div>
       <div className="AboutContent-interests">
         <h2>Interests</h2>
         <p>
-          out of coding, maths and learning, i like play videogrames which has
+          out of coding, maths and learning, i like play videogames which has
           history, i watch streams and i practice jiu-jitsu. I like chemistry
-          and psicology.
+          and psychology.
         </p>
       </div>
     </main>
