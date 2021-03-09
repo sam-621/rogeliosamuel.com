e95049db8b0667ea057b82cde1dcac4433a54bce
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-03-09 11:29:00 -0700

    change icons

--- src/assets/blogs.js
+++ src/assets/blogs.js
@@ -3,14 +3,14 @@ export const blogs = [
     title: '5 git commands you should know',
     date: '5 January 2021',
     icon:
-      'https://res.cloudinary.com/dnvp4s8pe/image/upload/v1615214112/My%20Website/git_ma8nsp.svg',
+      'https://res.cloudinary.com/dnvp4s8pe/image/upload/v1615314143/My%20Website/git_prrukm.svg',
     url: 'https://dev.to/rogeliosamuel621/5-git-commands-you-should-know-1o2g',
   },
   {
     title: 'React without create-react-app Webpack 5',
     date: '30 December 2020',
     icon:
-      'https://res.cloudinary.com/dnvp4s8pe/image/upload/v1606665660/My%20Website/webpack_xqbcwg.svg',
+      'https://res.cloudinary.com/dnvp4s8pe/image/upload/v1615314099/My%20Website/react_1_lnhgxj.svg',
     url:
       'https://dev.to/rogeliosamuel621/react-without-create-react-app-webpack-5-1b1o',
   },
