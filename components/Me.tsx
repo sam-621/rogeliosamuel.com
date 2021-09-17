b5a5da1bfed55b478aa23072feef7db4b2d6315e
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2021-09-17 14:26:41 -0600

    Image added and Me component created

---
+++ components/Me.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+
+const Me = () => {
+  return (
+    <Image
+      src='/Rogelio_Samuel_Moreno_Corrales'
+      width='100'
+      height='100'
+      layout='fixed'
+      alt='Rogelio Samuel Moreno Corrales'
+    />
+  )
+}
+
+export default Me
