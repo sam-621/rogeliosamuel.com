ff218503d2cea0266017a5988752967a758335da
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2021-09-22 20:46:14 -0600

    responsive design added to Header

--- components/containers/Header.tsx
+++ components/containers/Header.tsx
@@ -5,8 +5,12 @@ const Header = () => {
   return (
     <header className="mt-5">
       <Me />
-      <h1 className="text-center text-white mt-5 text-xl">Rogelio Samuel Moreno Corrales</h1>
-      <p className="text-center text-subtitle mt-1">Front-end developer at @EDteam | MERN stack</p>
+      <h1 className="text-center text-white mt-5 text-xl md:text-2xl lg:text-3xl xl">
+        Rogelio Samuel Moreno Corrales
+      </h1>
+      <p className="text-center text-subtitle mt-1 text-sm lg:text-base">
+        Front-end developer at @EDteam | MERN stack
+      </p>
       <SocialIcons />
     </header>
   )
