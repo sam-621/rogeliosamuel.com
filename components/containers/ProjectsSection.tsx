7c34d4f10a8beccb88ae8135d451100ad015644f
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2021-09-22 20:26:35 -0600

    image sizes bug fixed

--- components/containers/ProjectsSection.tsx
+++ components/containers/ProjectsSection.tsx
@@ -3,7 +3,7 @@ import ProjectCard from '../molecules/ProjectCard'
 
 const ProjectSection = () => {
   return (
-    <section className="grid gap-y-7 justify-center items-center md:grid-cols-2 md:gap-x-5 md:mx-5 lg:mx-32">
+    <section className="grid gap-y-7 justify-center md:grid-cols-2 md:gap-x-5 md:mx-5 lg:mx-32">
       {projects.map((item) => (
         <ProjectCard key={item.id} project={item} />
       ))}
