af50e003f980841e44c1526101bad05112011e8e
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2021-09-22 20:54:56 -0600

    Header styles changed

--- components/containers/Header.tsx
+++ components/containers/Header.tsx
@@ -3,15 +3,17 @@ import SocialIcons from '../molecules/SocialIcons'
 
 const Header = () => {
   return (
-    <header className="mt-5">
-      <Me />
-      <h1 className="text-center text-white mt-5 text-xl md:text-2xl lg:text-3xl xl">
-        Rogelio Samuel Moreno Corrales
-      </h1>
-      <p className="text-center text-subtitle mt-1 text-sm lg:text-base">
-        Front-end developer at @EDteam | MERN stack
-      </p>
-      <SocialIcons />
+    <header className="mt-5 h-screen grid items-center">
+      <div>
+        <Me />
+        <h1 className="text-center text-white mt-5 text-xl md:text-2xl lg:text-3xl xl">
+          Rogelio Samuel Moreno Corrales
+        </h1>
+        <p className="text-center text-subtitle mt-1 text-sm lg:text-base">
+          Front-end developer at @EDteam | MERN stack
+        </p>
+        <SocialIcons />
+      </div>
     </header>
   )
 }
