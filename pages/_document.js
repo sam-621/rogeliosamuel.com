630e1a595cc6a6309ded48df2ea0b4e515963120
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2021-09-25 15:57:20 -0600

    lang attribute added to html tag

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html>
+      <Html lang="es">
         <Head>
           <link rel="shortcut icon" href="/icons/logo.svg" />
           <meta name="author" content="Rogelio Samuel Moreno Corrales" />
