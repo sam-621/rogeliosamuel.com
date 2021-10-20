1c7909788a940289c69d730c1ef2cb06e4b2b345
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2021-10-19 19:27:35 -0600

    blogs data added

---
+++ data/blogs.ts
@@ -0,0 +1,13 @@
+export const blogs = [
+  {
+    img: '/images/fenix_blog.pmg',
+    title: 'Cómo crear una página web con ISR? y por qué lo usamos en EDteam',
+    description:
+      'En una de las campañas de EDteam tuvimos problemas con el rendimiento de nuestra web e identificamos 2 problemas.',
+  },
+  {
+    img: '/images/git_blog.png',
+    title: '5 git commands you should know',
+    description: `Here I'm going to list some git commands you should know, explain what they do, and use cases`,
+  },
+]
