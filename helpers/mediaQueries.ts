a7f6fdaba79764a5634bdfd9c3273abc9778cf01
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-09-04 10:43:10 -0600

    remove all files

--- helpers/mediaQueries.ts
+++
@@ -1,3 +0,0 @@
-export const mediaFrom768 = '(max-width: 768px)'
-export const mediaFrom1440 = '(max-width: 1080px)'
-export const mediaFrom530 = '(max-width: 530px)'
--- interfaces/project.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface TProject {
-  id: number
-  image: string
-  name: string
-  shortDescription: string
-  demo: string
-  repo: string
-}
