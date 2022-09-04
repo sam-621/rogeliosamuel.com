b9b869846a256826055a82ebbc600309385c476e
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-09-04 11:31:59 -0600

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        viewport: '#161616',
+        cards: '#212121',
+        primary: '#0066FF',
+        title: '#FFFFFF',
+        subtitle: '#B6B6B6'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
