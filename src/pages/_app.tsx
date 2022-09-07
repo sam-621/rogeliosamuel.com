1e4454d975947ca07b594f678ab517ccce8a2461
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-09-07 11:36:57 -0600

    change twitter card position

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { DefaultSEO } from '@containers/DefaultSEO'
 import { AppProps } from 'next/app'
+import Head from 'next/head'
 
 import 'tailwindcss/tailwind.css'
 import '../styles/utils.css'
@@ -8,6 +9,12 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <DefaultSEO />
+      <Head>
+        <meta
+          name="twitter:image"
+          content="https://res.cloudinary.com/dnvp4s8pe/image/upload/v1662571762/ijlizgmux2cqdmoijpih.png"
+        ></meta>
+      </Head>
       <Component {...pageProps} />
     </>
   )
