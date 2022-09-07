321c49874d889a0280e2e5fc329876e8c007b8a7
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-09-07 11:01:28 -0600

    add seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,16 @@
+import { DefaultSEO } from '@containers/DefaultSEO'
 import { AppProps } from 'next/app'
 
 import 'tailwindcss/tailwind.css'
 import '../styles/utils.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <DefaultSEO />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,37 +10,12 @@ class MyDocument extends Document {
     return (
       <Html lang="es">
         <Head>
-          <link rel="icon" href="/img/favicon.svg" />
-          <meta name="author" content="Rogelio Samuel Moreno Corrales" />
           <meta
             name="keywords"
             content="rogeliosamuel621, rogeliosamuel21, Rogelio Samuel Moreno Corrales, website"
           />
           <meta name="robots" content="index, follow" />
-          <meta
-            name="description"
-            content="Rogelio Samuel website. Here you can find my projects, experience level, and more details about me."
-          />
-
           <meta name="author" content="Rogelio Samuel Moreno Corrales" />
-          <meta
-            name="description"
-            content="My website created in Reactjs where you can find my projects, experience and something about me"
-          />
-          <meta name="keywords" content="Frontend, Reactjs, Web Development" />
-
-          {/* <!-- Twitter --> */}
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:creator" content="@rogeliosamuel21" />
-          <meta name="twitter:title" content="Rogelio Samuel website" />
-          <meta
-            name="twitter:description"
-            content="Here you can find my projects, experience and something about me"
-          />
-          <meta
-            name="twitter:image"
-            content="https://res.cloudinary.com/dnvp4s8pe/image/upload/v1607875701/My%20Website/my_image_oqkwgf.png"
-          />
         </Head>
         <body className="bg-viewport">
           <Main />
