4ee12f5e2e3fb4040c2c449dfaaf388a06541968
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-12-08 21:04:58 -0700

    add DefaultSEO

---
+++ src/data/copies.ts
@@ -0,0 +1,2 @@
+export const MAIN_DESCRIPTION =
+  'As a software developer i design, develop and deliver software products to improve the world with technology'
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
 import { AppProps } from 'next/app'
 
+import { DefaultSEO } from 'src/components'
+
 import '../styles/main.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <DefaultSEO />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
