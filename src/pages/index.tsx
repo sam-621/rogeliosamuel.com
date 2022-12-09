86fb16494541bfaa8b990e41715e12115e55ddf1
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-12-08 18:46:29 -0700

    update vscode settings

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,5 @@
 export const Home = () => {
-  return (
-    <>
-      <h1 className="text-red-600">Hola</h1>
-    </>
-  )
+  return <h1 className="text-red-600">Hola</h1>
 }
 
 export default Home
