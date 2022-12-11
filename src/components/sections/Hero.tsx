8b1b92b1c6f34d0e5099f20c40b864122f75306b
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-12-10 22:56:07 -0700

    feat: finish 1v of hero component

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -7,22 +7,22 @@ import { Title } from '../atoms/Title'
 export const Hero = () => {
   return (
     <>
-      <div className="absolute left-44 top-52">
+      <div className="absolute left-44 top-52 floating" style={{ animationDelay: '-2s' }}>
         <Image src="/icons/techs/react.svg" width={80} height={80} />
       </div>
-      <div className="absolute top-60 right-44">
+      <div className="absolute top-60 right-44 floating" style={{ animationDelay: '-1s' }}>
         <Image src="/icons/techs/nest.svg" width={80} height={80} />
       </div>
-      <div className="absolute left-52 bottom-40">
+      <div className="absolute left-52 bottom-40 floating" style={{ animationDelay: '-3s' }}>
         <Image src="/icons/techs/typescript.svg" width={152} height={80} />
       </div>
-      <div className="absolute bottom-32 right-56">
+      <div className="absolute bottom-32 right-56 floating" style={{ animationDelay: '-4s' }}>
         <Image src="/icons/techs/mongodb.svg" width={152} height={80} />
       </div>
       <div className="hero-height flex items-center justify-center backdrop-blur-sm">
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-4 opens">
           <Title className="md:text-center">Software developer</Title>
-          <Subtitle className="md:text-center">
+          <Subtitle className="md:text-center max-w-3xl">
             As a software developer i design, develop and deliver software products to improve the
             world with technology
           </Subtitle>
---
+++ src/interfaces/hero.ts
@@ -0,0 +1,4 @@
+export interface HeroTech {
+  url: string
+  width: number
+}
