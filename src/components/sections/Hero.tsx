773e7ba614672f44c7c3e59ff62c43a22fb45a32
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-12-10 21:06:47 -0700

    feat:  add sticky icons

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -7,8 +7,17 @@ import { Title } from '../atoms/Title'
 export const Hero = () => {
   return (
     <>
-      <div className="absolute">
-        <Image src="/icons/github.svg" width={24} height={24} />
+      <div className="absolute left-44 top-52">
+        <Image src="/icons/techs/react.svg" width={80} height={80} />
+      </div>
+      <div className="absolute top-60 right-44">
+        <Image src="/icons/techs/nest.svg" width={80} height={80} />
+      </div>
+      <div className="absolute left-52 bottom-40">
+        <Image src="/icons/techs/typescript.svg" width={152} height={80} />
+      </div>
+      <div className="absolute bottom-32 right-56">
+        <Image src="/icons/techs/mongodb.svg" width={152} height={80} />
       </div>
       <div className="hero-height flex items-center justify-center backdrop-blur-sm">
         <div className="flex flex-col gap-4">
