c9e16a6b01239d0d11aacd283eeb8ee1b5e480e1
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-12-10 20:45:04 -0700

    feat:  add media query to --nav-height css variable

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -6,19 +6,26 @@ import { Title } from '../atoms/Title'
 
 export const Hero = () => {
   return (
-    <div className="flex flex-col gap-4">
-      <Title className="md:text-center">Software developer</Title>
-      <Subtitle className="md:text-center">
-        As a software developer i design, develop and deliver software products to improve the world
-        with technology
-      </Subtitle>
-      <div className="flex gap-8 md:justify-center">
+    <>
+      <div className="absolute">
         <Image src="/icons/github.svg" width={24} height={24} />
-        <Image src="/icons/linkedin.svg" width={24} height={24} />
       </div>
-      <div className="mt-4 md:mx-auto">
-        <Button href="/CV.pdf">Download CV</Button>
+      <div className="hero-height flex items-center justify-center backdrop-blur-sm">
+        <div className="flex flex-col gap-4">
+          <Title className="md:text-center">Software developer</Title>
+          <Subtitle className="md:text-center">
+            As a software developer i design, develop and deliver software products to improve the
+            world with technology
+          </Subtitle>
+          <div className="flex gap-8 md:justify-center">
+            <Image src="/icons/github.svg" width={24} height={24} />
+            <Image src="/icons/linkedin.svg" width={24} height={24} />
+          </div>
+          <div className="mt-4 md:mx-auto">
+            <Button href="/CV.pdf">Download CV</Button>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ export const Home = () => {
   return (
     <>
       <Header />
-      <main className="mx-4 mt-12 md:mx-16 lg:mx-28">
+      <main className="mx-4 md:mx-16 lg:mx-28">
         <Hero />
       </main>
     </>
