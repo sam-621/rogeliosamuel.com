0780b5f8768d1df6b656a4825935110b90c2c45d
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-12-10 23:21:05 -0700

    feat:  add media query to heigh

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -7,20 +7,32 @@ import { Title } from '../atoms/Title'
 export const Hero = () => {
   return (
     <>
-      <div className="absolute left-44 top-52 floating" style={{ animationDelay: '-2s' }}>
+      <div
+        className="hidden md:block absolute left-44 top-52 floating"
+        style={{ animationDelay: '-2s' }}
+      >
         <Image src="/icons/techs/react.svg" width={80} height={80} />
       </div>
-      <div className="absolute top-60 right-44 floating" style={{ animationDelay: '-1s' }}>
+      <div
+        className="hidden md:block absolute top-60 right-44 floating"
+        style={{ animationDelay: '-1s' }}
+      >
         <Image src="/icons/techs/nest.svg" width={80} height={80} />
       </div>
-      <div className="absolute left-52 bottom-40 floating" style={{ animationDelay: '-3s' }}>
+      <div
+        className="hidden md:block absolute left-52 bottom-40 floating"
+        style={{ animationDelay: '-3s' }}
+      >
         <Image src="/icons/techs/typescript.svg" width={152} height={80} />
       </div>
-      <div className="absolute bottom-32 right-56 floating" style={{ animationDelay: '-4s' }}>
+      <div
+        className="hidden md:block absolute bottom-32 right-56 floating"
+        style={{ animationDelay: '-4s' }}
+      >
         <Image src="/icons/techs/mongodb.svg" width={152} height={80} />
       </div>
-      <div className="hero-height flex items-center justify-center backdrop-blur-sm">
-        <div className="flex flex-col gap-4 opens">
+      <div className="mt-14 flex items-center justify-center backdrop-blur-sm hero-height md:mt-0">
+        <div className="flex flex-col gap-4">
           <Title className="md:text-center">Software developer</Title>
           <Subtitle className="md:text-center max-w-3xl">
             As a software developer i design, develop and deliver software products to improve the
@@ -30,7 +42,7 @@ export const Hero = () => {
             <Image src="/icons/github.svg" width={24} height={24} />
             <Image src="/icons/linkedin.svg" width={24} height={24} />
           </div>
-          <div className="mt-4 md:mx-auto">
+          <div className="mt-8 md:mx-auto">
             <Button href="/CV.pdf">Download CV</Button>
           </div>
         </div>
