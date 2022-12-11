0cc36287a70caa7a06cb21a6201a91ff4832f9d7
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-12-11 13:16:09 -0700

    fix:  change cv url

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -6,7 +6,7 @@ export const Button: FC<Props> = ({ children, href, className }) => {
     return (
       <Link href={href}>
         <a
-          href=""
+          href={href}
           target="_blank"
           rel="noreferrer"
           className={`text-simple-title bg-primary rounded-lg py-3 px-6 text-base hover:bg-links transition-all ${className}`}
