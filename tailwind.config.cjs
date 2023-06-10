d7bd75b80ec4c520fd39705ca5405a1d45002801
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-06-10 13:44:20 -0700

    feat(components):  finish mbile hero

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,8 +9,8 @@ module.exports = {
       colors: {
         primary: '#146EF5',
         bg: '#000',
-        neutral1: '#E3E4E6',
-        neutral2: '#818181',
+        'neutral-1': '#E3E4E6',
+        'neutral-2': '#818181',
       },
       fontSize: {
         h1: 56,
@@ -18,7 +18,7 @@ module.exports = {
         h3: 32,
         h4: 24,
         h5: 16,
-        body1: 18,
+        'body-1': 18,
         button: 16,
         label: 14,
       },
