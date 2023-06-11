a0c0e77201ccc6143aa68a91d310919099f870f9
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-06-10 18:27:23 -0700

    feat(content):  add astro collections

---
+++ src/content/config.ts
@@ -0,0 +1,18 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection, z } from 'astro:content'
+// 2. Define your collection(s)
+const projectsCollection = defineCollection({
+  type: 'data',
+  schema: z.object({
+    image: z.string(),
+    title: z.string(),
+    description: z.string(),
+    type: z.string(),
+    preview: z.string(),
+  }),
+})
+// 3. Export a single `collections` object to register your collection(s)
+//    This key should match your collection directory name in "src/content"
+export const collections = {
+  projects: projectsCollection,
+}
