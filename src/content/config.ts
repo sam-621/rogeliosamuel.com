63ea168c979d0da9f251d622fdc9cb9f2f58b7e7
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-06-10 19:25:41 -0700

    feat(sections):  add blog section and blog card

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,5 @@
-// 1. Import utilities from `astro:content`
 import { defineCollection, z } from 'astro:content'
-// 2. Define your collection(s)
+
 const projectsCollection = defineCollection({
   type: 'data',
   schema: z.object({
@@ -12,8 +11,19 @@ const projectsCollection = defineCollection({
     order: z.number(),
   }),
 })
-// 3. Export a single `collections` object to register your collection(s)
-//    This key should match your collection directory name in "src/content"
+
+const blogsCollection = defineCollection({
+  type: 'data',
+  schema: z.object({
+    image: z.string(),
+    title: z.string(),
+    host: z.string(),
+    preview: z.string(),
+    order: z.number(),
+  }),
+})
+
 export const collections = {
   projects: projectsCollection,
+  blogs: blogsCollection,
 }
