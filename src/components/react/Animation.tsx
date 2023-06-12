bf8c44ccd50535a980b8b7e1d86a346c9e298d57
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-06-11 22:04:14 -0700

    feat(components):  add animations to each component

--- src/components/react/Animation.tsx
+++ src/components/react/Animation.tsx
@@ -1,17 +1,21 @@
-import { motion } from 'framer-motion'
+import { motion, MotionProps } from 'framer-motion'
 import type { FC, PropsWithChildren } from 'react'
 
-export const Animation: FC<Props> = ({ children, x = [], y = [] }) => {
+export const Animation: FC<Props> = ({ children, x = [], y = [], className }) => {
   const [fromX = 0, toX = 0] = x
   const [fromY = 0, toY = 0] = y
   return (
-    <motion.div
-      initial={{ opacity: 0, x: fromX, y: fromY }}
-      animate={{ opacity: 1, x: toX, y: toY }}
-      transition={{ type: 'spring' }}
-    >
-      {children}
-    </motion.div>
+    <div className=''>
+      <motion.div
+        initial={{ opacity: 0, x: fromX, y: fromY }}
+        whileInView={{ opacity: 1, x: toX, y: toY }}
+        transition={{ type: 'spring' }}
+        viewport={{ once: true, amount: 0.3 }}
+        className={className}
+      >
+        {children}
+      </motion.div>
+    </div>
   )
 }
 
@@ -24,4 +28,5 @@ type Props = PropsWithChildren & {
    * [fromX, toX]
    */
   x?: number[]
+  className?: string
 }
