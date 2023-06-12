533f821a1f37765e5c590ca80559eb9aa53ea7ad
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-06-12 00:19:26 -0700

    fix(analytics):  add analytics ina diferent way

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import react from '@astrojs/react'
 
-import react from "@astrojs/react";
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
-});
+  integrations: [tailwind(), react()],
+  output: 'server',
+  adapter: vercel({
+    analytics: true,
+  }),
+})
