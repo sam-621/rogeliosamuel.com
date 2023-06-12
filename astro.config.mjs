f721b75d064b1359c7b371cb5c56652c1edda161
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-06-12 00:22:22 -0700

    Revert "fix(analytics):  add analytics ina diferent way"
    This reverts commit 533f821a1f37765e5c590ca80559eb9aa53ea7ad.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,9 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-import react from '@astrojs/react'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
-import vercel from '@astrojs/vercel/serverless'
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  output: 'server',
-  adapter: vercel({
-    analytics: true,
-  }),
-})
+  integrations: [tailwind(), react()]
+});
