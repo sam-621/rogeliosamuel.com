9714596a33e02b150acb5ef45bd4f3113b18052f
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-07-22 15:12:57 -0700

    feat(tailwind):  add tailwind colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#146EF5',
+        'neutral-1': '#F1F1F1',
+        'neutral-2': '#818181',
+        view: '#000',
+      },
+    },
+  },
+  plugins: [],
 }
