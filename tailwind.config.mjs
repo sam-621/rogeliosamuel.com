4da2defe57f6ff1d450a45b1cc152cc9dc1d0e6f
samuel <samuel.corrales621@gmail.com>
2025-01-04 03:07:52 -0700

    add hero

---
+++ tailwind.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+      colors: {
+        background: '#EBEBEB',
+        foreground: '#383A37',
+        black: '#000000',
+        white: '#FFFFFF',
+        border: '#0000001A',
+        card: '#0000000D',
+      },
+      screens: {
+        '2xl': '1440px',
+      }
+    },
+	},
+	plugins: [],
+}
