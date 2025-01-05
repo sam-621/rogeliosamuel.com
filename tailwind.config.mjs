768b52565723080c5b6118c89baa94d632c011ee
samuel <samuel.corrales621@gmail.com>
2025-01-04 23:49:25 -0700

    add websql project page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,16 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: ['class'],
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
       colors: {
-        background: '#EBEBEB',
-        foreground: '#383A37',
-        black: '#000000',
-        'black-foreground': '#C7C5C8',
-        white: '#FFFFFF',
-        border: '#0000001A',
-        card: '#0000000D',
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        black: 'var(--black)',
+        'black-foreground': 'var(--black-foreground)',
+        white: 'var(--white)',
+        border: 'var(--border)',
+        card: 'var(--card)',
       },
       screens: {
         '2xl': '1440px',
