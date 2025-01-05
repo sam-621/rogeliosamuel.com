1fb8d6fec3a0551b7a5e18b75cc23ae9e30b7de3
samuel <samuel.corrales621@gmail.com>
2025-01-04 21:58:22 -0700

    add about me section

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,2 +1,6 @@
 export const wait = (ms: number) =>
   new Promise((resolve) => setTimeout(resolve, ms));
+
+export const isFirst = (index: number) => index === 0;
+export const isLast = (index: number, array: any[]) =>
+  index === array.length - 1;
