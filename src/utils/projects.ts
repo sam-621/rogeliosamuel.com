c252e0b204d41cb03a5ed20bf77a67db1558cc02
samuel <samuel.corrales621@gmail.com>
2025-01-05 18:51:24 -0700

    add more projects to work page

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -1,6 +1,9 @@
 import ImpotradeImage from "../assets/images/impotrade.png";
 import VendyxImage from "../assets/images/vendyx.png";
 import WSQLImage from "../assets/images/wsql.png";
+import AureoRealtyImage from "../assets/images/aureo-realty.png";
+import FitnesscorpImage from "../assets/images/fitnesscorp.png";
+import TicTacToeImage from "../assets/images/tic-tac-toe.webp";
 
 export const FEATURED_PROJECTS = [
   {
@@ -26,4 +29,26 @@ export const FEATURED_PROJECTS = [
   },
 ];
 
-export const ALL_PROJECTS = [...FEATURED_PROJECTS];
+export const SECONDARY_PROJECTS: typeof FEATURED_PROJECTS = [
+  {
+    name: "Aureo Realty",
+    image: AureoRealtyImage,
+    alt: "Aureo Realty",
+    techs: ["next", "nest", "ts", "aws", "prisma", "postgresql"],
+    href: "https://www.aureorealty.com/",
+  },
+  {
+    name: "Fitnesscorp",
+    image: FitnesscorpImage,
+    alt: "Fitnesscorp",
+    techs: ["next", "nest", "ts", "prisma", "postgresql"],
+    href: "https://sistema.fitnesscorp.com.mx/",
+  },
+  {
+    name: "Tic Tac Toe",
+    image: TicTacToeImage,
+    alt: "Tic Tac Toe",
+    techs: ["react", "ts", "socket-io"],
+    href: "https://tictactoe-multi.vercel.app/",
+  },
+];
