243b5c061c454282979bb80e4beed74732856d90
Samuel <samuel.corrales621@gmail.com>
2025-09-22 00:51:44 -0700

    feat: hero and info sections

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,24 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ['class'],
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
       colors: {
+        body: 'var(--body)',
         background: 'var(--background)',
         foreground: 'var(--foreground)',
-        black: 'var(--black)',
-        'black-foreground': 'var(--black-foreground)',
-        white: 'var(--white)',
+        subtitle: 'var(--subtitle)',
+        'subtitle-secondary': 'var(--subtitle-secondary)',
+        distinct: 'var(--distinct)',
         border: 'var(--border)',
-        card: 'var(--card)',
       },
       screens: {
         '2xl': '1440px',
-      }
+      },
     },
-	},
-	plugins: [
-    require('@tailwindcss/aspect-ratio')
-  ],
-}
+  },
+  plugins: [require('@tailwindcss/aspect-ratio')],
+};
